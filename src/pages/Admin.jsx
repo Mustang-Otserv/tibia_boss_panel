27c7a6b6b892256de1aa790bd035cb5c2728916e
unknown <pegasusotserv@gmail.com>
2025-08-15 06:55:12 -0300

    Inclusão de abas de navegação

---
+++ src/pages/Admin.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Admin() {
+  return (
+    <div>
+      <h1>Área Administrativa</h1>
+      <p>Somente para administradores.</p>
+    </div>
+  );
+}
---
+++ src/pages/Painel.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Painel() {
+  return (
+    <div>
+      <h1>Painel</h1>
+      <p>Conteúdo do painel vai aqui.</p>
+    </div>
+  );
+}
